cac0b85dd8479f53c05987e86823ee60789701d3
pikmr <benjahergar@gmail.com>
2018-07-26 23:41:54 +0200

    Ficheros que faltan - Picker y Viewmodel creación.

---
+++ AppSharedXamCETN/AppSharedXamCETN/ViewModels/PickerParaViewModel.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace AppCETN.ViewModels
+{
+    public class PickerParaViewModel<T> : INotifyPropertyChanged
+    {
+        public bool isChanged {get;set;}
+
+        List<T> listaItem;
+        public List<T> ListaItem
+        {
+            get { return listaItem; }
+            set
+            {
+                if (listaItem != value)
+                {
+                    listaItem = value;
+                    //OnPropertyChanged();
+                }
+            }
+        }
+
+
+        T selectedItem;
+        public T SelectedItem
+        {
+            get { return selectedItem; }
+            set
+            {
+                if(selectedItem != null && !selectedItem.Equals(value))
+                { 
+                  isChanged = true;
+                  selectedItem = value;
+                  OnPropertyChanged();
+                }
+                else
+                {
+                    if (selectedItem == null)
+                    {
+                        if (typeof(T).IsEquivalentTo(typeof(string)))
+                        {
+                            selectedItem = (T)(object)string.Empty;
+                        }
+                        else
+                        {
+                            selectedItem = (T)Activator.CreateInstance(typeof(T));
+                        }
+                    }
+                }
+            }
+        }
+
+        private int seleccionado;
+        public int SelectedIndex
+        {
+            get
+            {
+                seleccionado = listaItem.IndexOf(selectedItem);
+                return seleccionado;
+            }
+            set
+            {
+                if (seleccionado != value)
+                {
+                    seleccionado = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
---
+++ AppSharedXamCETN/AppSharedXamCETN/ViewModels/ViewModel1.cs
@@ -0,0 +1,64 @@
+﻿using AppCETN.Models;
+using AppCETN.Services;
+using AppSharedXamCETN.Shared;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Xamarin.Forms;
+using System.Linq;
+using System;
+
+namespace AppSharedXamCETN
+{
+    public class ViewModel1 : INotifyPropertyChanged
+    {
+        private ObservableCollection<Humano> _listaPantalla1;
+
+        public ObservableCollection<Humano> ListaPantalla1
+        {
+            get
+            {
+                return Singleton.Instance.Lista;
+            }
+            set { _listaPantalla1 = value;OnPropertyChanged(); }
+        }
+
+        public ViewModel1()
+        {
+            MessagingCenter.Subscribe<Views.EditHumanPage,Humano>(this, "update", (sender,arg) => {
+                //ListaPantalla1 = Singleton.Instance.Lista;3
+                var elem = ListaPantalla1.Single(x => x.IdEntidad == arg.IdEntidad);
+                ListaPantalla1.Remove(elem);
+                ListaPantalla1.Add(arg);
+            });
+            Add = LiteralesService.GetLiteral("lbl_agregar");
+         }
+
+        /*internal void ReloadOjo()
+        {
+            ObservableCollection<Humano> nueva_list = new ObservableCollection<Humano>();
+            ListaPantalla1.ToList().ForEach(p => { p.AtribSeleccionado = " -> " + p.Ojo; nueva_list.Add(p); });
+            ListaPantalla1.Clear();
+            nueva_list.ToList().ForEach(p => { ListaPantalla1.Add(p); });
+        }
+        internal void ReloadPrenda() {
+            ObservableCollection<Humano> nueva_list = new ObservableCollection<Humano>();
+            ListaPantalla1.ToList().ForEach(p => { p.AtribSeleccionado = " -> " + p.Prenda1; nueva_list.Add(p); });
+            ListaPantalla1.Clear();
+            nueva_list.ToList().ForEach(p => { ListaPantalla1.Add(p); });
+        }*/
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #region Elementos visuales
+        private string btnAdd = string.Empty;
+        public string Add 
+        {
+            get { return btnAdd; }
+            set { btnAdd = value; }
+        }
+        #endregion
+    }
+}
