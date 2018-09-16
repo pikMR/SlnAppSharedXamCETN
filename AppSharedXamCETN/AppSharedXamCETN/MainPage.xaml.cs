5cec176fd3598b95246d278db8bc6715c26277cf
BENJAMIN GARCIA HERNANDEZ <benjahergar@gmail.com>
2018-09-16 22:57:56 +0200

    Cambios para permitir habilitar/deshabilitar las bases legales, correcto funcionamiento.

--- AppSharedXamCETN/AppSharedXamCETN/MainPage.xaml.cs
+++ AppSharedXamCETN/AppSharedXamCETN/MainPage.xaml.cs
@@ -2,6 +2,8 @@
 using System;
 using AppCETN.Views;
 using AppCETN.Shared;
+using AppCETN.Services;
+using AppSharedXamCETN.Views;
 
 namespace AppCETN
 {
@@ -12,14 +14,6 @@ namespace AppCETN
          InitializeComponent();
          OnPropertyChanged("itemListViewModel");
          BindingContext = Singleton.Instance;
-            if (((Singleton)BindingContext).Lista == null || 
-                ((Singleton)BindingContext).Lista != null && ((Singleton)BindingContext).Lista.Count==0)
-            {
-                var res = DisplayAlert("Tienes la opción compartir tus datos si aceptas los terminos legales, si no tus datos no serán almacenados en una base de datos estadistica.", "Terminos", "Acepto los Terminos Legales","No acepto");
-
-                if (res.IsCompleted)
-                    Singleton.BasesLegales = true;
-            }
         }
 
         async void AddItem_Clicked(object sender, EventArgs e)
@@ -30,6 +24,37 @@ namespace AppCETN
         async void Save_Clicked(object sender, EventArgs e)
         {
             await Navigation.PopModalAsync();
+        }
+
+        private async void VerBasesLegales(object sender, EventArgs e)
+        {
+            string _basesLegales = LiteralesService.GetLiteral("lbl_bases");
+            string _acepto = LiteralesService.GetLiteral("lbl_acepto");
+            string _noacepto = LiteralesService.GetLiteral("lbl_no_acepto");
+            string _advertencia = LiteralesService.GetLiteral("lbl_adv");
+            string _leer = LiteralesService.GetLiteral("lbl_leer");
+
+            var action = await DisplayActionSheet(_basesLegales,_acepto,_noacepto,_advertencia,_leer);
+
+            if (action.Equals(_noacepto))
+            {
+                Singleton.BasesLegales = false;
+            }
+            else if(action.Equals(_leer) || action.Equals(_advertencia))
+            {
+                await Navigation.PushModalAsync(new NavigationPage(new ScrollTextView()));
+            }
+        }
+
+        protected override void OnAppearing()
+        {
+            if (ToolbarItems.Count == 0)
+            {
+                var legales = new ToolbarItem { Text = "Bases Legales", Icon = "legales.png"};
+                legales.Clicked += new EventHandler(VerBasesLegales);
+                ToolbarItems.Add(legales);
+            } 
+            base.OnAppearing();
         }
     }
 }
---
+++ AppSharedXamCETN/AppSharedXamCETN/Views/ScrollTextView.xaml.cs
@@ -0,0 +1,36 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppSharedXamCETN.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ScrollTextView : ContentPage
+	{
+		public ScrollTextView ()
+		{
+            InitializeComponent();
+            var stack = new StackLayout();
+            stack.Children.Add(new Editor { Text = AppCETN.Services.LiteralesService.GetLiteral("texto_bases_legales"), IsEnabled=false});
+            Button btnAcept = new Button { Text = AppCETN.Services.LiteralesService.GetLiteral("lbl_acepto") };
+            btnAcept.Clicked += new System.EventHandler(Aceptar);
+            Button btnCancelar = new Button { Text = AppCETN.Services.LiteralesService.GetLiteral("lbl_no_acepto") };
+            btnCancelar.Clicked += new System.EventHandler(Cancelar);
+            stack.Children.Add(btnAcept);
+            stack.Children.Add(btnCancelar);
+            Content = new ScrollView { Content = stack};
+        }
+
+        private async void Aceptar(object sender, System.EventArgs e)
+        {
+            await Navigation.PopModalAsync();
+            //base.OnBackButtonPressed();
+        }
+
+        private async void Cancelar(object sender, System.EventArgs e)
+        {
+            AppCETN.Shared.Singleton.BasesLegales = false;
+            await Navigation.PopModalAsync();
+            //base.OnBackButtonPressed();
+        }
+    }
+}
